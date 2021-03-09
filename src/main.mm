e22fc265e3db0b65ff5d4afb46755c03481b594c
Philipp Bauer <hello@philippbauer.org>
2021-03-09 02:27:49 -0600

    Removed AppMetrics

--- src/main.mm
+++ src/main.mm
@@ -1,51 +1,25 @@
 #include <iostream>
-#include <functional>
-#include <memory>
 
 #include "Photino/App/App.h"
-#include "PhotinoShared/Metrics.h"
 #include "PhotinoShared/Log.h"
 
 using namespace Photino;
 using namespace PhotinoShared;
 
-static Metrics AppMetrics;
-
-void *operator new(size_t size) _THROW_BAD_ALLOC
-{
-    AppMetrics.UsedInstances++;
-    AppMetrics.UsedMemory += size;
-    
-    return malloc(size);
-}
-
-void operator delete(void *memory, size_t size) _NOEXCEPT
-{
-    AppMetrics.FreedInstances++;
-    AppMetrics.FreedMemory += size;
-    
-    free(memory);
-}
-
 int main()
 {
     Log::WriteLine("Starting execution");
-    Log::WriteMetrics(&AppMetrics);
 
     App *app = new App();
-    Log::WriteMetrics(&AppMetrics);
-
     app
         ->Events()
         ->AddEventAction(AppEvents::WillRun, [](App *sender)
         {
             Log::WriteLine("Application is about to run.");
         });
-    Log::WriteMetrics(&AppMetrics);
 
+    // Main Window
     Window *mainWindow = new Window("Main Window");
-    Log::WriteMetrics(&AppMetrics);
-
     mainWindow
         ->Events()
         ->AddEventAction(WindowEvents::WindowDidResize, [](Window *sender)
@@ -64,31 +38,21 @@ int main()
         {
             Log::WriteLine("Window will close now.");
         });
-    Log::WriteMetrics(&AppMetrics);
 
     mainWindow
         ->WebView()
         ->LoadHtmlString("<html><body><h1>Hello Photino!</h1></body></html>");
-    Log::WriteMetrics(&AppMetrics);
-
-    Window *secondWindow = new Window("Second Window");
-    Log::WriteMetrics(&AppMetrics);
-
-    secondWindow
-        ->WebView()
-        ->LoadResource("http://www.tryphotino.io");
-    Log::WriteMetrics(&AppMetrics);
+    
+    // Second Window
+    // Window *secondWindow = new Window("Second Window");
+    // secondWindow
+    //     ->WebView()
+    //     ->LoadResource("http://www.tryphotino.io");
 
     // app->AddWindow(mainWindow)
     //    ->AddWindow(secondWindow);
-    // Log::WriteMetrics(&AppMetrics);
 
     app->Run();
-
-    // None of this is outside of app event loop
-    // and is never fired because the app is terminated.
-    delete app;
-    Log::WriteMetrics(&AppMetrics);
     
     Log::WriteLine("Stopping execution");
     return 0;
