151d03f2e728977d4ba0a33c5596385473a3660e
Philipp Bauer <hello@philippbauer.org>
2021-03-13 03:32:46 -0600

    Replaced manual alerts with Alert component

--- src/Photino/WebView/WebViewUiDelegate.mm
+++ src/Photino/WebView/WebViewUiDelegate.mm
@@ -1,3 +1,4 @@
+#include "../Alert/Alert.h"
 #include "WebViewUiDelegate.h"
 
 using namespace Photino;
@@ -18,21 +19,12 @@ using namespace Photino;
         std::string message = [scriptMessage UTF8String];
         photinoWebView->Events()->EmitEvent(WebViewEvents::OpenScriptAlert, &message);
 
-        NSAlert* alert = [[NSAlert alloc] init];
-
-        [alert setAlertStyle: NSAlertStyleInformational];
-        [alert setMessageText: @"Alert Message"];
-        [alert setInformativeText: [NSString stringWithUTF8String: message.c_str()]];
-        [alert addButtonWithTitle: @"OK"];
-
-        [alert
-            beginSheetModalForWindow: nativeWindow
-            completionHandler:^void (NSModalResponse response) {
-                photinoWebView->Events()->EmitEvent(WebViewEvents::CloseScriptAlert);
-                completionHandler();
-                [alert release];
-            }
-        ];
+        Alert *alert = new Alert(nativeWindow, message);
+        alert->Open([=](std::string response) -> void
+        {
+            photinoWebView->Events()->EmitEvent(WebViewEvents::CloseScriptAlert);
+            completionHandler();
+        });
     }
 
     - (void)webView:(WKWebView *)webView runJavaScriptConfirmPanelWithMessage:(NSString *)scriptMessage initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(BOOL result))completionHandler
@@ -43,32 +35,22 @@ using namespace Photino;
         std::string message = [scriptMessage UTF8String];
         photinoWebView->Events()->EmitEvent(WebViewEvents::OpenScriptConfirm, &message);
 
-        NSAlert* alert = [[NSAlert alloc] init];
-
-        [alert setMessageText: @"Please Confirm"];
-        [alert setInformativeText: [NSString stringWithUTF8String: message.c_str()]];
-        
-        [alert addButtonWithTitle:@"OK"];
-        [alert addButtonWithTitle:@"Cancel"];
-
-        [alert
-            beginSheetModalForWindow: nativeWindow
-            completionHandler:^void (NSModalResponse response) {
-                BOOL isConfirmed = response == NSAlertFirstButtonReturn;
-
-                // TODO:
-                // The EmitEvent only takes a std::string* for its
-                // second parameter. Use a string that evaluates to
-                // bool to get around that limitation. Eventually
-                // we want to have the option of passing more than
-                // a string type to the method and then this can
-                // be removed and replaced by the BOOL type.
-                std::string isConfirmedString = isConfirmed ? "true" : "false";
-                photinoWebView->Events()->EmitEvent(WebViewEvents::CloseScriptConfirm, &isConfirmedString);
-
-                completionHandler(isConfirmed);
-                [alert release];
-            }];
+        Alert *alert = new Alert(nativeWindow, message, "Please Confirm");
+        alert->AddButton("Cancel", "cancelled");
+        alert->Open([=](std::string response)
+        {
+            // TODO:
+            // The EmitEvent only takes a std::string* for its
+            // second parameter. Use a string that evaluates to
+            // bool to get around that limitation. Eventually
+            // we want to have the option of passing more than
+            // a string type to the method and then this can
+            // be removed and replaced by the BOOL type.
+            BOOL isConfirmed = response == "confirmed";
+            std::string isConfirmedString = isConfirmed ? "true" : "false";
+            photinoWebView->Events()->EmitEvent(WebViewEvents::CloseScriptConfirm, &isConfirmedString);
+            completionHandler(isConfirmed);
+        });
     }
 
     // - (void)webView:(WKWebView *)webView runJavaScriptTextInputPanelWithPrompt:(NSString *)prompt defaultText:(NSString *)defaultText initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(NSString *result))completionHandler
